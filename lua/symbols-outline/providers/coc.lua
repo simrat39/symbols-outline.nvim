a1bbef84b7c7240f88092c57732c5b8eb6f48234
simrat39 <simrats169169@gmail.com>
2021-10-06 14:22:18 -0700

    fix(coc): Fix COC installed check
    Fixes #70

--- lua/symbols-outline/providers/coc.lua
+++ lua/symbols-outline/providers/coc.lua
@@ -1,12 +1,9 @@
--- local config = require('symbols-outline.config')
-
 local M = {}
 
--- probably change this
 function M.should_use_provider(_)
     local coc_installed = vim.fn.exists("*CocActionAsync")
 
-    if not coc_installed then return end
+    if coc_installed == 0 then return end
 
     local coc_attached = vim.fn.call('CocAction', {'ensureDocument'})
     local has_symbols = vim.fn.call('CocHasProvider', {'documentSymbol'})
