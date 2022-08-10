336bc4b38c125ae2104a54759a6ffaa3edd85521
Simrat Grewal <simrats169169@gmail.com>
2022-08-10 15:13:33 -0700

    hover: Use new view api to get window

--- lua/symbols-outline/hover.lua
+++ lua/symbols-outline/hover.lua
@@ -1,6 +1,4 @@
-local vim = vim
-
-local main = require 'symbols-outline'
+local so = require 'symbols-outline'
 local util = vim.lsp.util
 local buf_request = require('symbols-outline.utils.lsp_utils').request
 
@@ -19,10 +17,10 @@ end
 
 -- handler yoinked from the default implementation
 function M.show_hover()
-  local current_line = vim.api.nvim_win_get_cursor(main.state.outline_win)[1]
-  local node = main.state.flattened_outline_items[current_line]
+  local current_line = vim.api.nvim_win_get_cursor(so.view.winnr)[1]
+  local node = so.state.flattened_outline_items[current_line]
 
-  local hover_params = get_hover_params(node, main.state.code_win)
+  local hover_params = get_hover_params(node, so.state.code_win)
 
   buf_request(hover_params.bufnr, 'textDocument/hover', hover_params, function(_, result, _, config)
     if not (result and result.contents) then
